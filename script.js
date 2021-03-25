251b192741c74a12c330187de5d112f7a59faf28
Aremtior <marcosleandrofl@gmail.com>
2021-03-25 12:31:46 -0300

    funçoes criadas no script js. Validar, genero personalizado e mostrar dados.

--- script.js
+++ script.js
@@ -1,15 +1,91 @@
-/* const generoPersonalizado = document.getElementById('other');
-generoPersonalizado.addEventListener('click', genPersonalizado);
-
-function genPersonalizado() {
-  const genPersonaliza = document.createElement('input');
-  genPersonaliza.type = 'text';
-  doc.appendChild;
-  /* genPersonaliza.insertBefore(document.getElementById('facebook-register')); */
-
 const btnLogin = document.getElementById('button-login');
 const entradaEmail = document.getElementById('user-email-phone');
 
 btnLogin.addEventListener('click', () => {
   alert(entradaEmail.value);
 });
+
+const firstname = document.getElementById('firstname');
+const lastname = document.getElementById('lastname');
+const phoneMail = document.getElementById('phone-email');
+const password = document.getElementById('password');
+const birthdate = document.getElementById('birthdate');
+let gender;
+// https://stackoverflow.com/questions/15839169/how-to-get-value-of-selected-radio-button
+
+// function customGender() {
+//   if (!document.getElementById('gender-custom')) {
+//     console.log('personalizado');
+//     const gender = document.createElement('input');
+//     gender.id = 'gender-custom';
+//     gender.type = 'text';
+//     gender.placeholder = 'Gênero (opcional)';
+//     gender.name = 'gender-custom';
+//     // document.querySelector('.right-content form').insertBefore(gender, document.getElementById('facebook-register'));
+//     document.getElementById('facebook-register').insertAdjacentElement("beforebegin", gender);
+//   }
+// }
+
+// https://stackoverflow.com/questions/8454079/toggle-displaynone-style-with-javascript
+function toggleDisplay(e) {
+  if (e.target.id === 'male' || e.target.id === 'female') {
+    document.getElementById('gender-custom').style.display = 'none';
+  } else {
+    document.getElementById('gender-custom').style.display = '';
+  }
+}
+
+function validate() {
+  gender = document.querySelector('input[name="gender"]:checked');
+  if (firstname.value && lastname.value && phoneMail.value && password.value && birthdate.value && gender) {
+    document.getElementById('error-message').style.display = 'none';
+    console.log('campos preenchidos');
+    return true;
+  } 
+  // else {
+    // document.getElementById('error-message').style.display = '';
+    // console.log('algum campo vazio');
+    // alert('Campos inválidos');
+    // return false;
+  // }
+}
+
+function clearRightContent() {
+  // https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
+  document.querySelector('.right-content').textContent = '';
+}
+
+function newContent(e) {
+  e.preventDefault();
+  if (validate()) {
+    const newContent = document.createElement('p');
+    newContent.id = 'new-content';
+    newContent.innerHTML = 'Olá, ';
+    newContent.innerHTML += firstname.value;
+    newContent.innerHTML += ' ' + lastname.value + '<br />';
+    newContent.innerHTML += ' ' + phoneMail.value + '<br />';
+    newContent.innerHTML += ' ' + birthdate.value + '<br />';
+    // https://stackoverflow.com/questions/15839169/how-to-get-value-of-selected-radio-button
+    newContent.innerHTML += gender.value;
+    clearRightContent();
+    document.querySelector('.right-content').appendChild(newContent);
+  } else {
+    document.getElementById('error-message').style.display = '';
+  }
+}
+
+document.getElementById('gender-custom').style.display = 'none';
+document.getElementById('error-message').style.display = 'none';
+
+// console.log(document.querySelector('input[name="gender"]:checked').value);
+const btnSubmit = document.getElementById('facebook-register');
+btnSubmit.addEventListener('click', newContent);
+
+const rdMale = document.getElementById('male');
+rdMale.addEventListener('change', toggleDisplay);
+
+const rdFemale = document.getElementById('female');
+rdFemale.addEventListener('change', toggleDisplay);
+
+const rdOther = document.getElementById('other');
+rdOther.addEventListener('change', toggleDisplay);
